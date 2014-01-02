a28b28fe5d9e960f7b10442072d2e4fe3d8c6974
Bill Cox <waywardgeek@gmail.com>
2014-01-01 19:50:11 -0500

    updates

--- keystretch.c
+++ keystretch.c
@@ -16,39 +16,84 @@
 typedef struct threadContextStruct *ThreadContext;
 
 struct threadContextStruct {
+    uint64 key[8];
+    uint64 lastPageData;
     uint64 *mem;
-    uint64 *threadKeys;
-    volatile uint32 *nextPageNumPtr; // This pointer points to the same value in each thread
-    ThreadContext nextContext;
     uint32 pageLength;
     uint32 numPages;
-    uint32 keyLength;
     uint32 cpuWorkMultiplier;
-    volatile bool spinLock;
 };
 
 // Fill toPage, hashing with the key and fromPage as we go.
-static void fillPage(ThreadContext c, uint64 *key, uint32 fromPageNum, uint32 toPageNum) {
+static void fillPage(ThreadContext c, uint32 fromPageNum, uint32 toPageNum) {
     uint32 pageLength = c->pageLength;
     uint64 *fromPage = c->mem + fromPageNum*pageLength;
     uint64 *toPage = c->mem + toPageNum*pageLength;
+    uint64 key0 = c->key[0];
+    uint64 key1 = c->key[1];
+    uint64 key2 = c->key[2];
+    uint64 key3 = c->key[3];
+    uint64 key4 = c->key[4];
+    uint64 key5 = c->key[5];
+    uint64 key6 = c->key[6];
+    uint64 key7 = c->key[7];
+    uint64 lastPageData =  c->lastPageData;
+    uint64 lastLastPageData =  c->lastPageData;
+    uint64 pageData0, pageData1, pageData2, pageData3;
+    uint64 pageData4, pageData5, pageData6, pageData7 = 0;
     uint32 pageMask = pageLength - 1;
-    uint64 keyData, pageData, lastPageData = 1;
-    uint32 numHashes = pageLength*c->cpuWorkMultiplier;
-    uint64 *k = key;
-    uint64 *kEnd = key + c->keyLength;
-    uint64 *t = toPage;
-    while(numHashes--) {
-        keyData = *k;
-        //printf("%llu\n", keyData);
-        *t++ = keyData;
-        pageData = fromPage[keyData & pageMask];
-        *k++ += (pageData*keyData) ^ lastPageData;
-        if(k == kEnd) {
-            k = key;
-        }
-        lastPageData = pageData;
+    uint32 numLoops = pageLength >> 3;
+    while(numLoops--) {
+        *toPage++ = key0;
+        *toPage++ = key1;
+        *toPage++ = key2;
+        *toPage++ = key3;
+        *toPage++ = key4;
+        *toPage++ = key5;
+        *toPage++ = key6;
+        *toPage++ = key7;
+
+/*
+        printf("%llu\n", key0);
+        printf("%llu\n", key1);
+        printf("%llu\n", key2);
+        printf("%llu\n", key3);
+        printf("%llu\n", key4);
+        printf("%llu\n", key5);
+        printf("%llu\n", key6);
+        printf("%llu\n", key7);
+*/
+
+        pageData0 = fromPage[key0 & pageMask];
+        pageData1 = fromPage[key1 & pageMask];
+        pageData2 = fromPage[key2 & pageMask];
+        pageData3 = fromPage[key3 & pageMask];
+        pageData4 = fromPage[key4 & pageMask];
+        pageData5 = fromPage[key5 & pageMask];
+        pageData6 = fromPage[key6 & pageMask];
+        pageData7 = fromPage[key7 & pageMask];
+
+
+        key0 += (pageData0*key1) ^ lastLastPageData;
+        key1 += (pageData1*key2) ^ pageData0;
+        key2 += (pageData2*key3) ^ pageData1;
+        key3 += (pageData3*key4) ^ pageData2;
+        key4 += (pageData4*key5) ^ pageData3;
+        key5 += (pageData5*key6) ^ pageData4;
+        key6 += (pageData6*key7) ^ pageData5;
+        key7 += (pageData7*key0) ^ pageData6;
+        lastLastPageData = lastPageData;
+        lastPageData = pageData7;
     }
+    c->key[0] = key0;
+    c->key[1] = key1;
+    c->key[2] = key2;
+    c->key[3] = key3;
+    c->key[4] = key4;
+    c->key[5] = key5;
+    c->key[6] = key6;
+    c->key[7] = key7;
+    c->lastPageData = pageData7;
 }
 
 // Hash pages randomly into the derived key.
@@ -57,23 +102,13 @@ static void *hashMem(void *threadContextPtr) {
     uint32 fromPageNum = 0;
     uint32 toPageNum;
     uint32 numPages = c->numPages;
-    uint32 hash = 0;
-    uint64 *key;
-    while(true) {
-        while(!c->spinLock);
-        c->spinLock = false;
-        toPageNum = (*(c->nextPageNumPtr))++;
-        c->nextContext->spinLock = true;
-        if(toPageNum >= numPages) {
-            pthread_exit(NULL);
-        }
-        key = c->threadKeys + (toPageNum & THREAD_MASK)*c->keyLength;
-        hash = (uint32)(key[0]);
-        if(toPageNum > MAX_THREADS) {
-            fromPageNum = hash % (toPageNum - MAX_THREADS);
-        }
-        fillPage(c, key, fromPageNum, toPageNum);
+    uint32 hash;
+    for(toPageNum = 1; toPageNum < numPages; toPageNum++) {
+        hash = c->key[0];
+        fromPageNum = hash % toPageNum;
+        fillPage(c, fromPageNum, toPageNum);
     }
+    pthread_exit(NULL);
 }
 
 /* This is the main key derivation function.  Parameters are:
@@ -108,21 +143,14 @@ bool keystretch(uint32 sha256HashRounds, uint32 cpuWorkMultiplier, uint64 memory
     uint32 numPages = (uint32)(memorySize/(pageLength*sizeof(uint64)));
     uint64 memoryLength = ((uint64)pageLength)*numPages;
     uint64 *mem = (uint64 *)malloc(memoryLength * sizeof(uint64));
-    uint32 keyLength = (derivedKeySize + 7)/8;
-    uint64 *threadKeys = (uint64 *)calloc(MAX_THREADS*keyLength, sizeof(uint64));
-    if(mem == NULL || threadKeys == NULL) {
+    if(mem == NULL) {
         fprintf(stderr, "Unable to allocate memory\n");
         return false;
     }
 
     // Initialize thread keys from derivedKey, and erase derivedKey
-    PBKDF2_SHA256(derivedKey, derivedKeySize, salt, saltSize, 1, (uint8 *)(void *)threadKeys,
-        MAX_THREADS*keyLength*sizeof(uint64));
+    PBKDF2_SHA256(derivedKey, derivedKeySize, salt, saltSize, 1, (uint8 *)(void *)mem, pageLength*sizeof(uint64));
     memset(derivedKey, '\0', derivedKeySize);
-
-    // Initialize the first page from the salt, without depending on the user's password
-    PBKDF2_SHA256((uint8 *)(void *)salt, saltSize, salt, saltSize, 1,
-        (uint8 *)(void *)mem, pageLength*sizeof(uint64));
 
     pthread_t threads[MAX_THREADS];
     struct threadContextStruct contexts[MAX_THREADS];
@@ -131,20 +159,15 @@ bool keystretch(uint32 sha256HashRounds, uint32 cpuWorkMultiplier, uint64 memory
     // with the spin-lock, and then passes it to the next round-robin.
     int rc;
     long t;
-    volatile uint32 nextPageNum = 1;
     for(t = 0; t < numThreads; t++) {
         c = contexts + t;
         c->mem = mem;
-        c->threadKeys = threadKeys;
-        c->keyLength = keyLength;
         c->pageLength = pageLength;
         c->numPages = numPages;
         c->cpuWorkMultiplier = cpuWorkMultiplier;
-        c->nextPageNumPtr = &nextPageNum;
-        c->nextContext = c + 1;
+        PBKDF2_SHA256((uint8 *)(void *)(mem + t*8*sizeof(uint64)), 8*sizeof(uint64), salt, saltSize, 1,
+            (uint8 *)(void *)(c->key + t), 8*sizeof(uint64));
     }
-    c->nextContext = contexts;
-    contexts[0].spinLock = true;
     for(t = 0; t < numThreads; t++) {
         c = contexts + t;
         rc = pthread_create(&threads[t], NULL, hashMem, (void *)c);
@@ -158,10 +181,9 @@ bool keystretch(uint32 sha256HashRounds, uint32 cpuWorkMultiplier, uint64 memory
         (void)pthread_join(threads[t], NULL);
     }
 
-    // Hash derived keys together and clear the thread keys
-    PBKDF2_SHA256((uint8 *)(void *)threadKeys, MAX_THREADS*keyLength*sizeof(uint64), salt, saltSize, 1,
+    // Hash the last page to form the key.
+    PBKDF2_SHA256((uint8 *)(void *)(mem + (numPages-1)*pageLength), pageLength*sizeof(uint64), salt, saltSize, 1,
         derivedKey, derivedKeySize);
-    memset(threadKeys, '\0', MAX_THREADS*keyLength*sizeof(uint64));
     memset(contexts, '\0', MAX_THREADS*sizeof(struct threadContextStruct));
 
     // Clear used memory if requested.  This slows down the code by about 1/3.
@@ -170,7 +192,6 @@ bool keystretch(uint32 sha256HashRounds, uint32 cpuWorkMultiplier, uint64 memory
     }
     if(freeMemory) {
         free(mem);
-        free(threadKeys);
     }
 
     return true;
